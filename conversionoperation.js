1056e98ab7b4122441fa2213303a9a4f3386d6f4
Jpsingh02 <adblockchain@gmail.com>
2024-06-26 13:11:25 +0000

    container config done

--- conversionoperation.js
+++ conversionoperation.js
@@ -1,16 +1,18 @@
-  // let score = 33 // Number
+  // let score = 33 // 33 (Number)
 
-// let score = "33" // String
+// let score = "33" // 33 (String)
 
-// let score = "33abc" // NaN
+// let score = "33abc" // NaN (String) Beacuse of "" This
 
-// let score = null // 0
+// let score = null // 0 (object)
 
-// let score = undefined // NaN
+// let score = undefined // NaN (undefined)
 
 // let score = true // 1 (boolean)
 
 // let score = false // 0 (boolean)
+
+// let score = "Jitender" // NaN (String)
 
 console.log(typeof score);
 console.log(typeof (score));
