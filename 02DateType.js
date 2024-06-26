f0e3beb894dfe643e7ed528ece3bdd8f982b04b8
Jpsingh02 <adblockchain@gmail.com>
2024-06-26 12:16:49 +0000

    container config done

--- 02DateType.js
+++ 02DateType.js
@@ -9,3 +9,23 @@ console.log(3+3); // Code Readability that should be high
 
 console.log("Jitender")
 
+
+
+let name = "Jitender" // String Data Types
+let age = 36 // Value Data Types
+let isLoggedIn = true or False // Boolean Data Type
+
+
+/*
+Data Types
+
+1. Numbers => 2 to Power 53
+2. bigInt
+3. String
+4. Boolean => Yes or No (true or False)
+5. null => Standalone Value  (Empty)
+6. 
+
+
+
+*/
