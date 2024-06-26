d110f4d355a97972a90c8482d93650858c9cc3dd
Jpsingh02 <adblockchain@gmail.com>
2024-06-26 12:05:20 +0000

    Container Config Done

--- 02DateType.js
+++ 02DateType.js
@@ -2,4 +2,10 @@
 
 // alert(3+3) we are using nodejs, not browser
 
-console.log(3+3); console.log("Jitender")
+
+
+
+console.log(3+3); // Code Readability that should be high
+
+console.log("Jitender")
+
