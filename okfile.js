f87d36499258d742f2309d0233e3f3db68470b49
Jpsingh02 <adblockchain@gmail.com>
2024-06-26 11:26:02 +0000

    containter config done

--- okfile.js
+++ okfile.js
@@ -5,5 +5,13 @@ accountCity = "Delhi"
 
 // accountId 2 //
 
+
+accountEmail = "jit@gmail.com"
+accountPassword = "457854"
+accountCity = "jaipur"
+
+/*
+comments
+*/
 console.log (accountId);
 console.table([accountId, accountEmail, accountPassword, accountCity,]);
